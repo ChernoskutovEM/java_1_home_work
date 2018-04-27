760a1b999431d3228a6f9c445e313858d7c8b654
Chernoskutov Evgeny <37318930+ChernoskutovEM@users.noreply.github.com>
2018-04-27 21:59:44 +0700

    Update Team.java

--- JavaCorelesson1/src/ru/geekbrains/java2/lesson1/l1/homework/Team.java
+++ JavaCorelesson1/src/ru/geekbrains/java2/lesson1/l1/homework/Team.java
@@ -1,4 +1,47 @@
 package ru.geekbrains.java2.lesson1.l1.homework;
 
-public class Team {
+import ru.geekbrains.java2.lesson1.l1.homework.competitors.Competitor;
+
+public class Team implements Competitor{
+
+    String type;
+    String name;
+    int maxRunDistance;
+    int maxJumpHeight;
+    int maxSwimDistance;
+    boolean onDistance;
+
+    public Team(String type, String name, int maxRunDistance, int maxJumpHeight, int maxSwimDistance, boolean onDistance) {
+        this.type = type;
+        this.name = name;
+        this.maxRunDistance = maxRunDistance;
+        this.maxJumpHeight = maxJumpHeight;
+        this.maxSwimDistance = maxSwimDistance;
+        this.onDistance = onDistance;
+    }
+
+    @Override
+    public void run(int distance) {
+
+    }
+
+    @Override
+    public void swim(int distance) {
+
+    }
+
+    @Override
+    public void jump(int height) {
+
+    }
+
+    @Override
+    public boolean isOnDistance() {
+        return false;
+    }
+
+    @Override
+    public void showResult() {
+
+    }
 }
